3bfffa63dae0e8a734b8b92d2874979a8ef3843e
TheBeardedBoss <footy_abu@hotmail.com>
2023-01-06 16:08:56 +0000

    Api working, however still need to fix search term as reponse is same always

--- app/src/main/java/com/example/atomicprogress/Model/Repository/Services/ExerciseService.java
+++ app/src/main/java/com/example/atomicprogress/Model/Repository/Services/ExerciseService.java
@@ -11,7 +11,7 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ExerciseService {
-    @Headers("X-Api-Key: JuSkjRpuHdq/LoEmMcJH+A==CzXnSiUaSSz0vQg7")
+    @Headers({"X-Api-Key: JuSkjRpuHdq/LoEmMcJH+A==CzXnSiUaSSz0vQg7","Accept: application/json"})
     @GET("/v1/exercises/")
-    Call<List<SearchResponse>> searchExercises(@Query("q") String SearchTerm);
+    Call<List<SearchResponse>> searchExercises(@Query("name")String SearchTerm);
 }
